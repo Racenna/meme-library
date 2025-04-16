b298d829d7a1a3436cc8ec5665af6b8685487bfd
Vlad Kosolapov <vlad.kosolapov48@gmail.com>
2025-04-16 16:39:52 +0300

    created clean nextjs project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,15 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/react";
 
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [heroui()],
-}
-
-module.exports = config;
+};
